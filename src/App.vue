795737f69819f319d5956354bd4be8ec88067cb7
Carwack <sybren.willemot@gmail.com>
2020-09-16 17:34:32 +0200

    Add change palette button and method

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@
     <c-reset />
     <!--Your application goes here -->
     <c-simple-grid :columns="4" zIndex="base">
-      <c-box bg="palettes.rosettesCream.primary-1" height="100vh"></c-box>
-      <c-box bg="palettes.rosettesCream.primary-2" height="100vh"></c-box>
-      <c-box bg="palettes.rosettesCream.primary-3" height="100vh"></c-box>
-      <c-box bg="palettes.rosettesCream.primary-4" height="100vh"></c-box>
+      <c-box :bg="`palettes.${currentPalette}.primary-1`" height="100vh"></c-box>
+      <c-box :bg="`palettes.${currentPalette}.primary-2`" height="100vh"></c-box>
+      <c-box :bg="`palettes.${currentPalette}.primary-3`" height="100vh"></c-box>
+      <c-box :bg="`palettes.${currentPalette}.primary-4`" height="100vh"></c-box>
     </c-simple-grid>
     <c-box zIndex="sticky" pos="absolute" top="50%" left="50%">
       <c-box pos="relative" top="-50%" left="-50%">
-        <c-button variant-color="blue" variant="outline">
+        <c-button variant-color="blue" variant="outline" @click="changePalette">
           Switch palette
         </c-button>
       </c-box>
@@ -21,6 +21,16 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      currentPalette: 'rosettesCream' // default palette to start with
+    }
+  },
+  methods: {
+    changePalette () {
+      this.currentPalette = 'bambooBeach'
+    }
+  }
 }
 </script>
