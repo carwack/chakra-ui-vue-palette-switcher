d2f13af458117feab6547febf530f4cb3474507e
Carwack <sybren.willemot@gmail.com>
2020-09-16 16:18:26 +0200

    Add a cute little blue button to switch palettes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,19 @@
   <c-theme-provider>
     <c-reset />
     <!--Your application goes here -->
-    <h1>Hello World</h1>
+    <c-simple-grid :columns="4" zIndex="base">
+      <c-box bg="palettes.rosettesCream.primary-1" height="100vh"></c-box>
+      <c-box bg="palettes.rosettesCream.primary-2" height="100vh"></c-box>
+      <c-box bg="palettes.rosettesCream.primary-3" height="100vh"></c-box>
+      <c-box bg="palettes.rosettesCream.primary-4" height="100vh"></c-box>
+    </c-simple-grid>
+    <c-box zIndex="sticky" pos="absolute" top="50%" left="50%">
+      <c-box pos="relative" top="-50%" left="-50%">
+        <c-button variant-color="blue" variant="outline">
+          Switch palette
+        </c-button>
+      </c-box>
+    </c-box>
   </c-theme-provider>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Chakra from '@chakra-ui/vue'
+import myAwesomeTheme from './assets/themes/my-awesome-theme'
 
-Vue.use(Chakra)
+Vue.use(Chakra, {
+  extendTheme: myAwesomeTheme
+})
 
 Vue.config.productionTip = false
 
